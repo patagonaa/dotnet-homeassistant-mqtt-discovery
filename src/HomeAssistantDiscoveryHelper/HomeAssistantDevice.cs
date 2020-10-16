1bb958d6d33d4b5821c14aa15e7ad250a07deda2
Patrick Krämer <patagonahn@gmail.com>
2020-10-16 15:45:10 +0200

    Initial Commit

---
+++ src/HomeAssistantDiscoveryHelper/HomeAssistantDevice.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace HomeAssistantDiscoveryHelper
+{
+    public class HomeAssistantDevice
+    {
+        [JsonProperty("identifiers")]
+        public IList<string> Identifiers { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+}
